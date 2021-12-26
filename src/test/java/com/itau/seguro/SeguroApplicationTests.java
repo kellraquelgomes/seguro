2c981ea637beb692ef768d408929a2a7bd5e0941
Raquel Gomes <kellraquel.gomes@gmail.com>
2021-12-26 18:44:37 -0300

    Criação inicial do projeto de Seguro

---
+++ src/test/java/com/itau/seguro/SeguroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.seguro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
