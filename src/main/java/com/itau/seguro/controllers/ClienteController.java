5a1bc98e44eeeb7e3bb1ee195d9a3b8c2a44a804
Raquel Gomes <kellraquel.gomes@gmail.com>
2022-01-02 18:19:38 -0300

    Criação Controller para consultar  os dados relacionados a um cliente (seu portfolio de produtos contratados) e seus respectivos acionamentos.

--- src/main/java/com/itau/seguro/controllers/ClienteController.java
+++ src/main/java/com/itau/seguro/controllers/ClienteController.java
@@ -2,15 +2,11 @@ package com.itau.seguro.controllers;
 
 import com.itau.seguro.dtos.ClienteDto;
 import com.itau.seguro.services.ClienteService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -26,6 +22,17 @@ public class ClienteController {
         clienteService.incluirClienteProduto(clienteDto);
 
         return new ResponseEntity<ClienteDto>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/clientes_seguros/{documento}/informacoes")
+    public ResponseEntity<ClienteDto> consultarClienteProdutosAcionamentos(@PathVariable(value = "documento") String documento) {
+
+        ClienteDto clienteDto = new ClienteDto();
+        clienteDto.setDocumento(documento);
+
+        ClienteDto clienteRetorno = clienteService.consultarClienteProdutosAcionamentos(clienteDto);
+
+        return new ResponseEntity<ClienteDto>(clienteRetorno, HttpStatus.OK);
     }
 
 }
