c170f9165c291025b075d71a8083641901601c1b
Raquel Gomes <kellraquel.gomes@gmail.com>
2022-01-02 16:16:45 -0300

    Correção Teste Unitário Cadastrar Acionamento de um Produto

--- src/main/java/com/itau/seguro/repositories/ClienteRepository.java
+++ src/main/java/com/itau/seguro/repositories/ClienteRepository.java
@@ -1,11 +1,9 @@
 package com.itau.seguro.repositories;
 
 import com.itau.seguro.models.Cliente;
-import com.itau.seguro.models.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
--- src/test/java/com/itau/seguro/services/impl/ClienteAcionamentoProdutoServiceImplTest.java
+++ src/test/java/com/itau/seguro/services/impl/ClienteAcionamentoProdutoServiceImplTest.java
@@ -33,50 +33,44 @@ public class ClienteAcionamentoProdutoServiceImplTest {
 
     @Test
     public void saveClienteAcionamentoProduto(){
+
         final ClienteAcionamentoProdutoServiceImpl service = new ClienteAcionamentoProdutoServiceImpl();
         final ClienteAcionamentoProdutoRepository repository= context.mock(ClienteAcionamentoProdutoRepository.class);
         final ProdutoRepository repositoryProduto = context.mock(ProdutoRepository.class);
         final ClienteRepository repositoryCliente = context.mock(ClienteRepository.class);
+
         // parametros de entrada
         final ClienteAcionamentoProdutoDto clienteAcionamentoProdutoDto = new ClienteAcionamentoProdutoDto();
         final ClienteDto clienteDto = new ClienteDto();
-        final ProdutoDto produtoDto = new ProdutoDto();
         clienteDto.setClienteId(new Integer(1));
         clienteDto.setDocumento("28570368097");
+
+        final ProdutoDto produtoDto = new ProdutoDto();
         produtoDto.setProdutoId(new Integer(1));
+
         clienteAcionamentoProdutoDto.setCliente(clienteDto);
         clienteAcionamentoProdutoDto.setProduto(produtoDto);
         clienteAcionamentoProdutoDto.setDataAcionamento(DateUtils.converterStringParaDateTime(("2022-01-05")).toDate());
 
-        //Retorno mock
         final Optional<Cliente> cliente =  Optional.ofNullable(new Cliente());
-        cliente.get().setClienteId(clienteAcionamentoProdutoDto.getCliente().getClienteId());
+        cliente.get().setClienteId(new Integer(1));
         cliente.get().setDocumento(clienteAcionamentoProdutoDto.getCliente().getDocumento());
-        final Produto produto = new Produto();
-        produto.setProdutoId(clienteAcionamentoProdutoDto.getProduto().getProdutoId());
 
+        final  Optional< Produto> produto = Optional.ofNullable(new Produto());
+        produto.get().setProdutoId(new Integer(1));
+        produto.get().setQuantidadeAcionamento(new Integer(2));
 
-        final  Optional< Produto> produtoOptional = Optional.ofNullable(new Produto());
-        produtoOptional.get().setProdutoId(new Integer(1));
-        produtoOptional.get().setQuantidadeAcionamento(new Integer(2));
-
+        //retorno mock
         final List< ClienteAcionamentoProduto > clienteAcionamentoProdutos = new ArrayList<ClienteAcionamentoProduto>();
         ClienteAcionamentoProduto clienteAcionamentoProduto = new ClienteAcionamentoProduto();
         clienteAcionamentoProduto.setCliente(cliente.get());
-        clienteAcionamentoProduto.setProduto(produto);
+        clienteAcionamentoProduto.setProduto(produto.get());
         clienteAcionamentoProduto.setDataAcionamento(DateUtils.converterStringParaDateTime(("2021-12-05")).toDate());
         clienteAcionamentoProdutos.add(clienteAcionamentoProduto);
 
-        //retorno mock save
-
         final ClienteAcionamentoProduto clienteAcionamentoProdutoRetorno = new ClienteAcionamentoProduto();
-        final Cliente clienteRetorno = new Cliente();
-        clienteRetorno.setClienteId(new Integer(1));
-        final Produto produtoRetorno = new Produto();
-        produtoRetorno.setProdutoId(new Integer(1));
-
-        clienteAcionamentoProdutoRetorno.setCliente(clienteRetorno);
-        clienteAcionamentoProdutoRetorno.setProduto(produtoRetorno);
+        clienteAcionamentoProdutoRetorno.setCliente(cliente.get());
+        clienteAcionamentoProdutoRetorno.setProduto(produto.get());
         clienteAcionamentoProdutoRetorno.setDataAcionamento(DateUtils.converterStringParaDateTime(("2022-01-05")).toDate());
 
 
@@ -85,13 +79,15 @@ public class ClienteAcionamentoProdutoServiceImplTest {
                 oneOf(repositoryCliente).findByDocumento(with(clienteAcionamentoProdutoDto.getCliente().getDocumento()));
                 will(returnValue(cliente));
 
-                oneOf(repository).countByClienteAndProduto(with(cliente.get()),with(produto));
-                will(returnValue(new Integer(1)));
 
                 oneOf(repositoryProduto).findById(with(clienteAcionamentoProdutoDto.getProduto().getProdutoId()));
-                will(returnValue(produtoOptional));
+                will(returnValue(produto));
 
-                oneOf(repository).findByClienteAndProdutoOrderByDataAcionamentoDesc(with(cliente.get()), with(produto));
+                oneOf(repository).countByClienteAndProduto(with(cliente.get()),with(produto.get()));
+                will(returnValue(new Integer(1)));
+
+
+                oneOf(repository).findByClienteAndProdutoOrderByDataAcionamentoDesc(with(cliente.get()), with(produto.get()));
                 will(returnValue(clienteAcionamentoProdutos));
 
                 oneOf(repository).save(with(clienteAcionamentoProdutoRetorno));
@@ -109,38 +105,37 @@ public class ClienteAcionamentoProdutoServiceImplTest {
 
     @Test
     public void testVerificarProdutoLimiteAcionamento(){
+
         final ClienteAcionamentoProdutoServiceImpl service = new ClienteAcionamentoProdutoServiceImpl();
         final ClienteAcionamentoProdutoRepository repository= context.mock(ClienteAcionamentoProdutoRepository.class);
         final ProdutoRepository repositoryProduto = context.mock(ProdutoRepository.class);
+
         // parametros de entrada
         final ClienteAcionamentoProdutoDto clienteAcionamentoProdutoDto = new ClienteAcionamentoProdutoDto();
+
         final ClienteDto clienteDto = new ClienteDto();
-        final ProdutoDto produtoDto = new ProdutoDto();
-        clienteDto.setClienteId(new Integer(1));
         clienteDto.setDocumento("28570368097");
+
+        final ProdutoDto produtoDto = new ProdutoDto();
         produtoDto.setProdutoId(new Integer(1));
+
         clienteAcionamentoProdutoDto.setCliente(clienteDto);
         clienteAcionamentoProdutoDto.setProduto(produtoDto);
         clienteAcionamentoProdutoDto.setDataAcionamento(DateUtils.converterStringParaDateTime("2022-01-04").toDate());
 
         final Optional<Cliente> cliente =  Optional.ofNullable(new Cliente());
-        cliente.get().setClienteId(clienteAcionamentoProdutoDto.getCliente().getClienteId());
-        cliente.get().setDocumento(clienteAcionamentoProdutoDto.getCliente().getDocumento());
+        cliente.get().setClienteId(new Integer(1));
+        cliente.get().setDocumento(clienteDto.getDocumento());
 
-        //Retorno mock
         final  Optional< Produto> produto = Optional.ofNullable(new Produto());
         produto.get().setProdutoId(new Integer(1));
         produto.get().setQuantidadeAcionamento(new Integer(2));
-
 
 
         context.checking(new Expectations() {
             {
                 oneOf(repository).countByClienteAndProduto(with(cliente.get()),with(produto.get()));
                 will(returnValue(new Integer(2)));
-
-                oneOf(repositoryProduto).findById(with(produto.get().getProdutoId()));
-                will(returnValue(produto));
 
                 never(repository).findByClienteAndProdutoOrderByDataAcionamentoDesc(with(any(Cliente.class)),with(any(Produto.class)));
                 will(returnValue(null));
@@ -162,39 +157,43 @@ public class ClienteAcionamentoProdutoServiceImplTest {
 
     @Test
     public void testVerificarPeriodoAcionamentoProdutoCliente(){
+
         final ClienteAcionamentoProdutoServiceImpl service = new ClienteAcionamentoProdutoServiceImpl();
         final ClienteAcionamentoProdutoRepository repository= context.mock(ClienteAcionamentoProdutoRepository.class);
         final ProdutoRepository repositoryProduto = context.mock(ProdutoRepository.class);
+
         // parametros de entrada
         final ClienteAcionamentoProdutoDto clienteAcionamentoProdutoDto = new ClienteAcionamentoProdutoDto();
+
         final ClienteDto clienteDto = new ClienteDto();
+        clienteDto.setDocumento("28570368097");
+
         final ProdutoDto produtoDto = new ProdutoDto();
-        clienteDto.setClienteId(new Integer(1));
         produtoDto.setProdutoId(new Integer(1));
+
         clienteAcionamentoProdutoDto.setCliente(clienteDto);
         clienteAcionamentoProdutoDto.setProduto(produtoDto);
         clienteAcionamentoProdutoDto.setDataAcionamento(DateUtils.converterStringParaDateTime(("2022-01-04")).toDate());
 
-        //Retorno mock
-        final  Optional< Produto> produtoOptional = Optional.ofNullable(new Produto());
-        produtoOptional.get().setProdutoId(new Integer(1));
-        produtoOptional.get().setQuantidadeAcionamento(new Integer(2));
+        final Optional<Cliente> cliente =  Optional.ofNullable(new Cliente());
+        cliente.get().setClienteId(new Integer(1));
+        cliente.get().setDocumento(clienteDto.getDocumento());
 
+        final  Optional< Produto> produto = Optional.ofNullable(new Produto());
+        produto.get().setProdutoId(new Integer(1));
+        produto.get().setQuantidadeAcionamento(new Integer(2));
+
+        //Retorno mock
         final List< ClienteAcionamentoProduto > clienteAcionamentoProdutos = new ArrayList<ClienteAcionamentoProduto>();
         ClienteAcionamentoProduto clienteAcionamentoProduto = new ClienteAcionamentoProduto();
-
-        final Cliente cliente = new Cliente();
-        cliente.setClienteId(clienteAcionamentoProdutoDto.getCliente().getClienteId());
-        final Produto produto = new Produto();
-        produto.setProdutoId(clienteAcionamentoProdutoDto.getProduto().getProdutoId());
-        clienteAcionamentoProduto.setCliente(cliente);
-        clienteAcionamentoProduto.setProduto(produto);
+        clienteAcionamentoProduto.setCliente(cliente.get());
+        clienteAcionamentoProduto.setProduto(produto.get());
         clienteAcionamentoProduto.setDataAcionamento(DateUtils.converterStringParaDateTime(("2021-12-05")).toDate());
         clienteAcionamentoProdutos.add(clienteAcionamentoProduto);
 
         context.checking(new Expectations() {
             {
-                oneOf(repository).findByClienteAndProdutoOrderByDataAcionamentoDesc(with(cliente), with(produto));
+                oneOf(repository).findByClienteAndProdutoOrderByDataAcionamentoDesc(with(cliente.get()), with(produto.get()));
                 will(returnValue(clienteAcionamentoProdutos));
 
             }
@@ -203,7 +202,7 @@ public class ClienteAcionamentoProdutoServiceImplTest {
         service.setProdutoRepository(repositoryProduto);
 
         try {
-            service.verificarPeriodoAcionamentoProdutoCliente(clienteAcionamentoProdutoDto,cliente,produto);
+            service.verificarPeriodoAcionamentoProdutoCliente(clienteAcionamentoProdutoDto,cliente.get(),produto.get());
         } catch (BusinessException b) {
             context.assertIsSatisfied();
             assertEquals("O período entre os acionamentos do mesmo produtos, é de no mínimo " +
@@ -213,56 +212,56 @@ public class ClienteAcionamentoProdutoServiceImplTest {
         fail("Nao lancou exception");
     }
 
-    @Test
-    public void testProdutoNaoCadastrado(){
-        final ClienteAcionamentoProdutoServiceImpl service = new ClienteAcionamentoProdutoServiceImpl();
-        final ClienteAcionamentoProdutoRepository repository= context.mock(ClienteAcionamentoProdutoRepository.class);
-        final ProdutoRepository repositoryProduto = context.mock(ProdutoRepository.class);
-        // parametros de entrada
-        final ClienteAcionamentoProdutoDto clienteAcionamentoProdutoDto = new ClienteAcionamentoProdutoDto();
-        final ClienteDto clienteDto = new ClienteDto();
-        final ProdutoDto produtoDto = new ProdutoDto();
-        clienteDto.setClienteId(new Integer(1));
-        produtoDto.setProdutoId(new Integer(1));
-        clienteAcionamentoProdutoDto.setCliente(clienteDto);
-        clienteAcionamentoProdutoDto.setProduto(produtoDto);
-        clienteAcionamentoProdutoDto.setDataAcionamento(DateUtils.converterStringParaDateTime("2022-01-04").toDate());
-
-        final Cliente cliente = new Cliente();
-        cliente.setClienteId(clienteAcionamentoProdutoDto.getCliente().getClienteId());
-        final Produto produto = new Produto();
-        produto.setProdutoId(clienteAcionamentoProdutoDto.getProduto().getProdutoId());
-
-        //Retorno mock
-        final  Optional< Produto> produtoOptional = Optional.ofNullable(new Produto());
-        produtoOptional.get().setProdutoId(new Integer(1));
-        produtoOptional.get().setQuantidadeAcionamento(new Integer(2));
-
-        context.checking(new Expectations() {
-            {
-                oneOf(repository).countByClienteAndProduto(with(cliente),with(produto));
-                will(returnValue(new Integer(2)));
-
-                oneOf(repositoryProduto).findById(with(clienteAcionamentoProdutoDto.getProduto().getProdutoId()));
-                will(returnValue(produtoOptional));
-
-                never(repository).findByClienteAndProdutoOrderByDataAcionamentoDesc(with(any(Cliente.class)),with(any(Produto.class)));
-                will(returnValue(null));
-
-            }
-        });
-        service.setClienteAcionamentoProdutoRepository(repository);
-        service.setProdutoRepository(repositoryProduto);
-
-        try {
-            service.verificarProdutoLimiteAcionamento(clienteAcionamentoProdutoDto,cliente,produto);
-        } catch (BusinessException b) {
-            context.assertIsSatisfied();
-            assertEquals("Não é possivel acionar o seguro,o número de acionamento está no limite.", b.getMessage());
-            return;
-        }
-        fail("Nao lancou exception");
-    }
-
+//    @Test
+//    public void testProdutoNaoCadastrado(){
+//        final ClienteAcionamentoProdutoServiceImpl service = new ClienteAcionamentoProdutoServiceImpl();
+//        final ClienteAcionamentoProdutoRepository repository= context.mock(ClienteAcionamentoProdutoRepository.class);
+//        final ProdutoRepository repositoryProduto = context.mock(ProdutoRepository.class);
+//        // parametros de entrada
+//        final ClienteAcionamentoProdutoDto clienteAcionamentoProdutoDto = new ClienteAcionamentoProdutoDto();
+//        final ClienteDto clienteDto = new ClienteDto();
+//        final ProdutoDto produtoDto = new ProdutoDto();
+//        clienteDto.setClienteId(new Integer(1));
+//        produtoDto.setProdutoId(new Integer(1));
+//        clienteAcionamentoProdutoDto.setCliente(clienteDto);
+//        clienteAcionamentoProdutoDto.setProduto(produtoDto);
+//        clienteAcionamentoProdutoDto.setDataAcionamento(DateUtils.converterStringParaDateTime("2022-01-04").toDate());
+//
+//        final Cliente cliente = new Cliente();
+//        cliente.setClienteId(clienteAcionamentoProdutoDto.getCliente().getClienteId());
+//        final Produto produto = new Produto();
+//        produto.setProdutoId(clienteAcionamentoProdutoDto.getProduto().getProdutoId());
+//
+//        //Retorno mock
+//        final  Optional< Produto> produtoOptional = Optional.ofNullable(new Produto());
+//        produtoOptional.get().setProdutoId(new Integer(1));
+//        produtoOptional.get().setQuantidadeAcionamento(new Integer(2));
+//
+//        context.checking(new Expectations() {
+//            {
+//                oneOf(repository).countByClienteAndProduto(with(cliente),with(produto));
+//                will(returnValue(new Integer(2)));
+//
+//                oneOf(repositoryProduto).findById(with(clienteAcionamentoProdutoDto.getProduto().getProdutoId()));
+//                will(returnValue(produtoOptional));
+//
+//                never(repository).findByClienteAndProdutoOrderByDataAcionamentoDesc(with(any(Cliente.class)),with(any(Produto.class)));
+//                will(returnValue(null));
+//
+//            }
+//        });
+//        service.setClienteAcionamentoProdutoRepository(repository);
+//        service.setProdutoRepository(repositoryProduto);
+//
+//        try {
+//            service.verificarProdutoLimiteAcionamento(clienteAcionamentoProdutoDto,cliente,produto);
+//        } catch (BusinessException b) {
+//            context.assertIsSatisfied();
+//            assertEquals("Não é possivel acionar o seguro,o número de acionamento está no limite.", b.getMessage());
+//            return;
+//        }
+//        fail("Nao lancou exception");
+//    }
+//
 
 }
